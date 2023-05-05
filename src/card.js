4f47b0fbf3816558ffe3fe66643b104592fba4c2
thethomasam <samkoshythomas007@gmail.com>
2023-05-05 10:43:52 +0930

    added like button

--- src/card.js
+++ src/card.js
@@ -1,29 +1,31 @@
 import React from 'react'
-
+import Like from './llike_button'
 export default function Card({ image, title, desc }) {
     return (
-        <div>
 
-            <li class="cards_item">
-                <div class="card">
-                    <div class="card_image">
-                        <img src={image} />
-                        <span class="card_price"><span>$</span>9</span>
-                    </div>
-                    <div class="card_content">
-                        <h2 class="card_title"> {title}</h2>
-                        <div class="card_text">
 
-                            <p> {desc}
-                            </p>
-                            <hr />
-
-                        </div>
-                    </div>
+        <li class="cards_item">
+            <div class="card">
+                <div class="card_image">
+                    <img src={image} />
+                    <span class="card_price"><span>$</span>9</span>
                 </div>
-            </li>
+                <div class="card_content">
+                    <h2 class="card_title"> {title}</h2>
+                    <div class="card_text">
+
+                        <p> {desc}
+                        </p>
+                        <hr />
+                        <Like />
+                    </div>
+
+                </div>
+            </div>
+
+        </li>
 
 
-        </div>
+
     )
 }
---
+++ src/llike_button.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Like() {
+    return (
+        <div> <button class="dislike">
+            like
+        </button></div>
+    )
+}
